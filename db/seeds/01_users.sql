5af7b358c5ffe2e5369a2e237b1181991946dc6e
William Perry <durabilliam@gamil.com>
2021-05-01 16:54:09 -0230

    add initial seeds

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,5 +1,5 @@
 -- Users table seeds here (Example)
-INSERT INTO users (name, email, password, favorite_id, map_id, photo_url, admin)
-VALUES ('Will E Makeit', 'bathroom99@toilet.com', '123', 2, 3, 'https://images.pexels.com/photos/47547/squirrel-animal-cute-rodents-47547.jpeg?auto=compress&cs=tinysrgb&h=350', TRUE),
-('Kent C. Strate', 'spectacles@eyepatch.ca', '123', 2, 1, 'https://images.pexels.com/photos/257558/pexels-photo-257558.jpeg?auto=compress&cs=tinysrgb&h=350', FALSE),
-('Amanda Kiss', 'Ohmy@smootch.com', '123', 1, 2, 'https://images.pexels.com/photos/45201/kitty-cat-kitten-pet-45201.jpeg?auto=compress&cs=tinysrgb&h=350', FALSE);
+INSERT INTO users (name, email, password, photo_url, admin)
+VALUES ('Will E Makeit', 'bathroom99@toilet.com', '123', 'https://images.pexels.com/photos/47547/squirrel-animal-cute-rodents-47547.jpeg?auto=compress&cs=tinysrgb&h=350', TRUE),
+('Kent C. Strate', 'spectacles@eyepatch.ca', '123', 'https://images.pexels.com/photos/257558/pexels-photo-257558.jpeg?auto=compress&cs=tinysrgb&h=350', FALSE),
+('Amanda Kiss', 'Ohmy@smootch.com', '123', 'https://images.pexels.com/photos/45201/kitty-cat-kitten-pet-45201.jpeg?auto=compress&cs=tinysrgb&h=350', FALSE);
--- db/seeds/05_widgets.sql
+++ db/seeds/05_widgets.sql
@@ -4,20 +4,20 @@ INSERT INTO widgets (name, user_id) VALUES ('Chains', 2);
 INSERT INTO widgets (name, user_id) VALUES ('Bearings', 2);
 
 
-INSERT INTO maps (user_id, favorite_id, title, description, photo_url, city, lat, long,)
-VALUES (1, 3, "Downtown Montreal", "This is a description", 'https://image.shutterstock.com/image-photo/french-onion-gratin-soup-260nw-104052002.jpg', Montreal, 1.2, 0.9),
-(2, 1, "Downtown Toronto", "This is a description", 'https://image.shutterstock.com/image-photo/two-fresh-submarine-sandwiches-ham-260nw-497930494.jpg', Toronto, 1.3, 0.4),
-(3, 2, "Downtown Halifax", "This is a description", 'https://image.shutterstock.com/image-photo/dark-chocolate-stack-chips-powder-260nw-230301934.jpg', Halifax, 1.5, 0.3);
+INSERT INTO maps (user_id, title, description, photo_url, city, lat, long)
+VALUES (1, 'Downtown Montreal', 'This is a description', 'https://image.shutterstock.com/image-photo/french-onion-gratin-soup-260nw-104052002.jpg', 'Montreal', 1.2, 0.9),
+(2, 'Toronto', 'This is a description', 'https://image.shutterstock.com/image-photo/two-fresh-submarine-sandwiches-ham-260nw-497930494.jpg', 'Toronto', 1.3, 0.4),
+(3, 'Halifax', 'This is a description', 'https://image.shutterstock.com/image-photo/dark-chocolate-stack-chips-powder-260nw-230301934.jpg', 'Halifax', 1.5, 0.3);
 
 INSERT INTO points (map_id, title, description, photo_url)
-VALUES (1,"Soup Palace", "The best French Onion Soup!!", 'https://image.shutterstock.com/image-photo/french-onion-gratin-soup-260nw-104052002.jpg'),
-(2, "Sandwich Palace", "The best Sandwich Ever!!", 'https://image.shutterstock.com/image-photo/two-fresh-submarine-sandwiches-ham-260nw-497930494.jpg'),
-(3, "Chocloate Palace", "The best Chocolate!!", 'https://image.shutterstock.com/image-photo/dark-chocolate-stack-chips-powder-260nw-230301934.jpg');
+VALUES (1, 'Soup Palace', 'The best French Onion Soup!!', 'https://image.shutterstock.com/image-photo/french-onion-gratin-soup-260nw-104052002.jpg'),
+(2, 'Sandwich Palace', 'The best Sandwich Ever!!', 'https://image.shutterstock.com/image-photo/two-fresh-submarine-sandwiches-ham-260nw-497930494.jpg'),
+(3, 'Chocloate Palace', 'The best Chocolate!!', 'https://image.shutterstock.com/image-photo/dark-chocolate-stack-chips-powder-260nw-230301934.jpg');
 
-INSERT INTO favories (map_id, user_id)
+INSERT INTO favorites (map_id, user_id)
 VALUES (1, 1),
-VALUES (2, 1),
-VALUES (3, 1),
-VALUES (1, 2),
-VALUES (2, 2),
-VALUES (3, 3),
+(2, 1),
+(3, 1),
+(1, 2),
+(2, 2),
+(3, 3);
