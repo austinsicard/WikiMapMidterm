f3a2c2ad45add60481619f9a96cea84547b985f1
William Perry <durabilliam@gamil.com>
2021-05-03 19:37:45 -0230

    qc helpers

--- routes/helperFunctions.js
+++ routes/helperFunctions.js
@@ -18,7 +18,7 @@ const pool = new Pool({
 });
 
 //to get a user by email
-const getUserWithEmail = (email) => {
+const getUserByEmail = (email) => {
 
   const queryString = `SELECT * FROM users WHERE email = $1;`;
   const values = [email];
@@ -29,11 +29,11 @@ const getUserWithEmail = (email) => {
     .catch((err) => null);
 };
 
-exports.getUserWithEmail = getUserWithEmail;
+exports.getUserByEmail = getUserByEmail;
 
 
 //to get a user by user_id
-const getUserWithId = (id) => {
+const getUserById = (id) => {
   const queryString = `SELECT * FROM users WHERE id = $1;`;
   const values = [id];
   return pool
@@ -42,7 +42,7 @@ const getUserWithId = (id) => {
     .catch((err) => err.message);
 };
 
-exports.getUserWithId = getUserWithId;
+exports.getUserById = getUserById;
 
 //ADD New User, only registered users can create and edit maps and points
 const addUser =  ({name, password, email}) => {
@@ -58,24 +58,27 @@ const addUser =  ({name, password, email}) => {
 exports.addUser = addUser;
 
 //To Select map properties from map_id
-const getMapPropertiesFromMapId =(map_id) =>{
+const getMapById =(map_id) =>{
 
   const queryString = `SELECT * FROM maps WHERE maps.id = $1;`;
   const values = [map_id];
   return pool
     .query(queryString, values)
-    .then((result) => result.row[0])
+    .then((result) => result.rows[0])
     .catch((err) => err.message);
 };
 
-exports.getMapPropertiesFromMapId = getMapPropertiesFromMapId;
+exports.getMapById = getMapById;
 
 
 
 //To Select the favorites (map_id for a user)
-const getUserFavoritesWithId = (id) => {
+const getFavoritesByUser = (id) => {
 
-  const queryString = `SELECT map_id FROM favorites WHERE user_id = $1;`;
+  const queryString = `SELECT maps.* FROM maps
+  JOIN favorites ON maps.id = map_id
+  JOIN users ON users.id = favorites.user_id
+  WHERE users.id = favorites.user_id AND users.id = $1;`
   const values = [id];
 
   return pool
@@ -84,11 +87,11 @@ const getUserFavoritesWithId = (id) => {
     .catch((err) => null);
 };
 
-exports.getUserFavoritesWithId = getUserFavoritesWithId;
+exports.getFavoritesByUser = getFavoritesByUser;
 
 
 //TO select map_id created by user
-const getUserMapsWithId = (id) => {
+const getMapsByUser = (id) => {
 
   const queryString = `SELECT maps.id FROM maps WHERE user_id = $1;`;
   const values = [id];
@@ -99,11 +102,11 @@ const getUserMapsWithId = (id) => {
     .catch((err) => null);
 };
 
-exports.getUserMapsWithId = getUserMapsWithId;
+exports.getMapsByUser = getMapsByUser;
 
 
 //TO select points created by user
-const getUserPointsWithId = (id) => {
+const getPointsByUser = (id) => {
 
   const queryString = `SELECT points.id FROM points WHERE user_id = $1;`;
   const values = [id];
@@ -114,13 +117,15 @@ const getUserPointsWithId = (id) => {
     .catch((err) => null);
 };
 
-exports.getUserPointsWithId = getUserPointsWithId;
+exports.getPointsByUser = getPointsByUser;
 
-//TO select map_id from points
-const getMapsFromPoints = (point_id) => {
+//TO select map info from user id through user points
+const getMapsByPoints = (user_id) => {
 
-  const queryString = `SELECT map_id FROM points WHERE points.id = $1;`;
-  const values = [point_id];
+  const queryString = `SELECT maps.*
+    FROM maps JOIN points ON maps.id = map_id
+    JOIN users ON users.id = points.user_id WHERE users.id = $1;`;
+  const values = [user_id];
 
   return pool
     .query(queryString, values)
@@ -128,10 +133,10 @@ const getMapsFromPoints = (point_id) => {
     .catch((err) => null);
 };
 
-exports.getMapsFromPoints = getMapsFromPoints;
+exports.getMapsByPoints = getMapsByPoints;
 
 //Create favorites
-const createFavorite =  ({map_id, user_id}) => {
+const addFavorite =  ({map_id, user_id}) => {
 
   const queryString = `INSERT INTO favorites (map_id, user_id VALUES ($1, $2) RETURNING *;`;
   const values = [map_id, user_id];
@@ -141,10 +146,10 @@ const createFavorite =  ({map_id, user_id}) => {
     .catch((err) => err.message);
 };
 
-exports.createFavorite = createFavorite;
+exports.addFavorite = addFavorite;
 
 //Create points
-const createPoint =  ({map_id, user_id, title, description, photo_url, lat, long}) => {
+const addPoint =  ({map_id, user_id, title, description, photo_url, lat, long}) => {
 
   const queryString = `INSERT INTO points (map_id, user_id, title, description, photo_url, lat, long) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *;`;
   const values = [map_id, user_id, title, description, photo_url, lat, long];
@@ -154,10 +159,77 @@ const createPoint =  ({map_id, user_id, title, description, photo_url, lat, long
     .catch((err) => err.message);
 };
 
-exports.createPoint = createPoint;
+exports.addPoint = addPoint;
+
+const editPoint = (options) => {
+  const queryParams = [];
+  let queryString = `UPDATE points SET `;
+  //To change Point title
+  if(options.title){
+    queryParams.push(`%${options.title}%`);
+    if (queryString.length > 1) {
+      queryString += `, title = $${queryParams.length}`;
+    } else {
+      queryString += `title = $${queryParams.length}`;
+    }
+  }
+  //to Change Point description
+  if(options.description){
+    queryParams.push(`%${options.description}%`);
+    if (queryString.length > 1) {
+      queryString += `, description = $${queryParams.length}`;
+    } else {
+      queryString += `description = $${queryParams.length}`;
+    }
+  }
+  //to Change Point Photo
+  if(options.photo_url){
+    queryParams.push(`%${options.photo_url}%`);
+    if (queryString.length > 1) {
+      queryString += `, photo_url = $${queryParams.length}`;
+    } else {
+      queryString += `photo_url = $${queryParams.length}`;
+    }
+  }
+  //to Change Point Latitude
+  if(options.lat){
+    queryParams.push(`%${options.lat}%`);
+    if (queryString.length > 1) {
+      queryString += `, lat = $${queryParams.length}`;
+    } else {
+      queryString += `lat = $${queryParams.length}`;
+    }
+  }
+  //to Change Point Longitude
+  if(options.long){
+  queryParams.push(`%${options.long}%`);
+  if (queryString.length > 1) {
+     queryString += `, long = $${queryParams.length}`;
+   } else {
+     queryString += `long = $${queryParams.length}`;
+   }
+  }
+  queryParams.push(`%${options.points.id}%`);
+  queryString += `WHERE points.id = $${queryParams.length}`;
+  queryParams.push(`%${options.user_id}%`);
+  queryString += `AND user_id = $${queryParams.length};`;
+
+// UPDATE Customers
+// SET ContactName = 'Alfred Schmidt', City= 'Frankfurt'
+// WHERE CustomerID = 1;
+
+  return pool
+    .query(queryString, queryParams)
+    .then((result) => result.rows)
+    .catch((err) => err.message);
+};
+
+exports.editPoint = editPoint;
+
+
 
 //Create maps
-const createMap =  ({user_id, title, description, photo_url, city, lat, long}) => {
+const addMap =  ({user_id, title, description, photo_url, city, lat, long}) => {
 
   const queryString = `INSERT INTO maps (user_id, title, description, photo_url, city, lat, long) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *;`;
   const values = [user_id, title, description, photo_url, city, lat, long];
@@ -167,12 +239,12 @@ const createMap =  ({user_id, title, description, photo_url, city, lat, long}) =
     .catch((err) => err.message);
 };
 
-exports.createMap = createMap;
+exports.addMap = addMap;
 
 //Delete a map
-const deleteMap =  ({user_id, map_id}) => {
+const deleteMap =  ({map_id}) => {
 
-  const queryString = `DELETE FROM maps WHERE user_id = $1 AND maps.id = $2) VALUES ($1, $2);`;
+  const queryString = `DELETE FROM maps WHERE maps.id = $1) VALUES ($1);`;
   const values = [user_id, map_id];
   return pool
     .query(queryString, values)
@@ -181,6 +253,43 @@ const deleteMap =  ({user_id, map_id}) => {
 };
 
 exports.deleteMap = deleteMap;
+
+const editMap = (options) => {
+  const queryParams = [];
+  let queryString = `UPDATE maps SET `;
+  //To change Map title
+  if(options.title){
+    queryParams.push(`%${options.title}%`);
+    if (queryString.length > 1) {
+      queryString += `, title = $${queryParams.length}`;
+    } else {
+      queryString += `title = $${queryParams.length}`;
+    }
+  }
+  //to Change Map description
+  if(options.description){
+    queryParams.push(`%${options.description}%`);
+    if (queryString.length > 1) {
+      queryString += `, description = $${queryParams.length}`;
+    } else {
+      queryString += `description = $${queryParams.length}`;
+    }
+  }
+  queryParams.push(`%${options.maps.id}%`);
+  queryString += `WHERE maps.id = $${queryParams.length}`;
+  queryParams.push(`%${options.user_id}%`);
+  queryString += `AND user_id = $${queryParams.length};`;
+// UPDATE Customers
+// SET ContactName = 'Alfred Schmidt', City= 'Frankfurt'
+// WHERE CustomerID = 1;
+  return pool
+    .query(queryString, queryParams)
+    .then((result) => result.rows)
+    .catch((err) => err.message);
+};
+
+exports.editMap = editMap;
+
 
 //Delete a point
 const deletePoint =  ({user_id, point_id}) => {
