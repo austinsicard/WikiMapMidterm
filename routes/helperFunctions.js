ffc69b2083c0895e1e676cd65e8c15b2a3761ed3
Nelli Butenko <butenkonelly93@gmail.com>
2021-05-03 21:53:48 +0000

    Fix functions

--- routes/helperFunctions.js
+++ routes/helperFunctions.js
@@ -1,55 +1,52 @@
-// load .env data into process.env
-require('dotenv').config();
+// // load .env data into process.env
+// require('dotenv').config();
 
-// save settings from env file in variable
-const user = process.env.DB_USER;
-const password = process.env.DB_PASS;
-const host = process.env.DB_HOST;
-const database = process.env.DB_NAME;
+// // save settings from env file in variable
+// const user = process.env.DB_USER;
+// const password = process.env.DB_PASS;
+// const host = process.env.DB_HOST;
+// const database = process.env.DB_NAME;
 
-// connection with database
-const { Pool } = require('pg');
-// pass in the varibles from env (using shortcut)
-const pool = new Pool({
-  user,
-  password,
-  host,
-  database
-});
+// // connection with database
+// const { Pool } = require('pg');
+// // pass in the varibles from env (using shortcut)
+// const pool = new Pool({
+//   user,
+//   password,
+//   host,
+//   database
+// });
 
+// FUNCTIONS
+// USERS
 //to get a user by email
-const getUserWithEmail = (email) => {
-
+const getUserByEmail = (db, email) => {
   const queryString = `SELECT * FROM users WHERE email = $1;`;
   const values = [email];
-
-  return pool
+  return db
     .query(queryString, values)
     .then((result) => result.rows[0])
     .catch((err) => null);
 };
-
-exports.getUserWithEmail = getUserWithEmail;
-
+exports.getUserByEmail = getUserByEmail;
 
 //to get a user by user_id
-const getUserWithId = (id) => {
+const getUserById = (db, id) => {
   const queryString = `SELECT * FROM users WHERE id = $1;`;
   const values = [id];
-  return pool
+  return db
     .query(queryString, values)
     .then((result) => result.rows[0])
     .catch((err) => err.message);
 };
 
-exports.getUserWithId = getUserWithId;
+exports.getUserById = getUserById;
 
 //ADD New User, only registered users can create and edit maps and points
-const addUser =  ({name, password, email}) => {
-
+const addUser =  (db, {name, password, email}) => {
   const queryString = `INSERT INTO users (name, password, email) VALUES ($1, $2, $3) RETURNING *;`;
   const values = [name, password, email];
-  return pool
+  return db
     .query(queryString, values)
     .then((result) => result.rows[0])
     .catch((err) => err.message);
@@ -57,124 +54,140 @@ const addUser =  ({name, password, email}) => {
 
 exports.addUser = addUser;
 
-//To Select map properties from map_id
-const getMapPropertiesFromMapId =(map_id) =>{
 
+
+
+// WIDGETS
+
+// list maps
+const listMaps = (db, options, limit = 10) => {
+  const sql = `SELECT title FROM maps`
+  db.query(sql)
+    .then()
+};
+exports.listMaps = listMaps;
+
+// Individual map
+const getMapById = (db, map_id) => {
   const queryString = `SELECT * FROM maps WHERE maps.id = $1;`;
   const values = [map_id];
-  return pool
+  return db
     .query(queryString, values)
-    .then((result) => result.row[0])
+    .then((result) => result.rows[0])
     .catch((err) => err.message);
 };
-
-exports.getMapPropertiesFromMapId = getMapPropertiesFromMapId;
-
+exports.getMapById= getMapById;
 
 
-//To Select the favorites (map_id for a user)
-const getUserFavoritesWithId = (id) => {
 
-  const queryString = `SELECT map_id FROM favorites WHERE user_id = $1;`;
+// list favourites
+const getFavoritesByUser = (db, id) => {
+  //const queryString = `SELECT map_id FROM favorites WHERE user_id = $1;`;
+  const queryString = `
+  SELECT maps.* 
+  FROM maps
+  JOIN favorites ON maps.id = map_id
+  JOIN users ON users.id = favorites.user_id
+  WHERE user.id = favorites.user_id
+  AND users.id = $1;
+  `;
   const values = [id];
-
-  return pool
+  return db
     .query(queryString, values)
     .then((result) => result.rows[0])
     .catch((err) => null);
 };
 
-exports.getUserFavoritesWithId = getUserFavoritesWithId;
+exports.getFavoritesByUser = getFavoritesByUser;
 
 
 //TO select map_id created by user
-const getUserMapsWithId = (id) => {
+const getMapsByUser = (db, id) => {
 
-  const queryString = `SELECT maps.id FROM maps WHERE user_id = $1;`;
+  const queryString = `SELECT maps.* FROM maps WHERE user_id = $1;`;
   const values = [id];
 
-  return pool
+  return db
     .query(queryString, values)
-    .then((result) => result.rows[0])
+    .then((result) => result.rows)
     .catch((err) => null);
 };
 
-exports.getUserMapsWithId = getUserMapsWithId;
+exports.getMapsByUser = getMapsByUser;
 
 
 //TO select points created by user
-const getUserPointsWithId = (id) => {
+// const getPointsByUser = (db, id) => {
+//   const queryString = `SELECT points.id FROM points WHERE user_id = $1;`;
+//   const values = [id];
+//   return db
+//     .query(queryString, values)
+//     .then((result) => result.rows[0])
+//     .catch((err) => null);
+// };
+// exports.getPointsByUser = getPointsByUser;
 
-  const queryString = `SELECT points.id FROM points WHERE user_id = $1;`;
-  const values = [id];
-
-  return pool
+// maps, user contributed to
+const getMapsByPoints = (db, user_id ) => {
+  const queryString = `
+  SELECT maps.* 
+  FROM maps 
+  JOIN points ON maps.id = map_id 
+  JOIN users ON users.id = points.user_id 
+  WHERE points.user_id = users.id
+  AND users.id = $1;`;
+  const values = [user_id];
+  return db
     .query(queryString, values)
     .then((result) => result.rows[0])
     .catch((err) => null);
 };
 
-exports.getUserPointsWithId = getUserPointsWithId;
+exports.getMapsByPoints = getMapsByPoints;
 
-//TO select map_id from points
-const getMapsFromPoints = (point_id) => {
-
-  const queryString = `SELECT map_id FROM points WHERE points.id = $1;`;
-  const values = [point_id];
-
-  return pool
-    .query(queryString, values)
-    .then((result) => result.rows[0])
-    .catch((err) => null);
-};
-
-exports.getMapsFromPoints = getMapsFromPoints;
-
-//Create favorites
-const createFavorite =  ({map_id, user_id}) => {
-
+//Add to favorites
+const addFavorite =  (db, map_id, user_id) => {
   const queryString = `INSERT INTO favorites (map_id, user_id VALUES ($1, $2) RETURNING *;`;
   const values = [map_id, user_id];
-  return pool
+  return db
     .query(queryString, values)
     .then((result) => result.rows[0])
     .catch((err) => err.message);
 };
-
-exports.createFavorite = createFavorite;
+exports.addFavorite = addFavorite;
 
 //Create points
-const createPoint =  ({map_id, user_id, title, description, photo_url, lat, long}) => {
+const addPoint =  (db, {map_id, user_id, title, description, photo_url, lat, long}) => {
 
   const queryString = `INSERT INTO points (map_id, user_id, title, description, photo_url, lat, long) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *;`;
   const values = [map_id, user_id, title, description, photo_url, lat, long];
-  return pool
+  return db
     .query(queryString, values)
     .then((result) => result.rows[0])
     .catch((err) => err.message);
 };
 
-exports.createPoint = createPoint;
+exports.addPoint = addPoint;
 
 //Create maps
-const createMap =  ({user_id, title, description, photo_url, city, lat, long}) => {
+const addMap =  (db, {user_id, title, description, photo_url, city, lat, long}) => {
 
   const queryString = `INSERT INTO maps (user_id, title, description, photo_url, city, lat, long) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *;`;
   const values = [user_id, title, description, photo_url, city, lat, long];
-  return pool
+  return db
     .query(queryString, values)
     .then((result) => result.rows[0])
     .catch((err) => err.message);
 };
 
-exports.createMap = createMap;
+exports.addMap = addMap;
 
 //Delete a map
-const deleteMap =  ({user_id, map_id}) => {
+const deleteMap =  (db, map_id) => {
 
   const queryString = `DELETE FROM maps WHERE user_id = $1 AND maps.id = $2) VALUES ($1, $2);`;
   const values = [user_id, map_id];
-  return pool
+  return db
     .query(queryString, values)
     .then((result) => result.rows[0])
     .catch((err) => err.message);
@@ -183,11 +196,11 @@ const deleteMap =  ({user_id, map_id}) => {
 exports.deleteMap = deleteMap;
 
 //Delete a point
-const deletePoint =  ({user_id, point_id}) => {
+const deletePoint =  (db, {user_id, point_id}) => { // add map_id?
 
   const queryString = `DELETE FROM points WHERE user_id = $1 AND points.id = $2) VALUES ($1, $2);`;
   const values = [user_id, point_id];
-  return pool
+  return db
     .query(queryString, values)
     .then((result) => result.rows[0])
     .catch((err) => err.message);
@@ -196,11 +209,11 @@ const deletePoint =  ({user_id, point_id}) => {
 exports.deletePoint = deletePoint;
 
 //Delete a favorite
-const deleteFavorite =  ({user_id, map_id}) => {
+const deleteFavorite =  (db, user_id, map_id) => {
 
   const queryString = `DELETE FROM favorites WHERE user_id = $1 AND map_id = $2) VALUES ($1, $2);`;
   const values = [user_id, map_id];
-  return pool
+  return db
     .query(queryString, values)
     .then((result) => result.rows[0])
     .catch((err) => err.message);
