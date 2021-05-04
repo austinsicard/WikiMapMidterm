cd95a32b5fcaab3df34a749903f96157c2a9a04d
Nelli Butenko <butenkonelly93@gmail.com>
2021-05-04 16:21:21 +0000

    Use jQuery to connect to database on main page

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,10 +1,36 @@
-$(() => {
-  $.ajax({
-    method: "GET",
-    url: "/api/users"
-  }).done((users) => {
-    for(user of users) {
-      $("<div>").text(user.name).appendTo($("body"));
-    }
-  });;
-});
+// pull out the maps from server database
+$(document).ready(function () { // should be the only entry point
+  loadMaps();
+})
+
+const loadMaps = function () {
+  console.log('function executing')
+  $.ajax('/maps', { method: 'GET' })
+    .then(data => {
+      renderMaps(data);
+    })
+    .catch(err => {
+    })
+};
+
+// loop through the array of map objects
+const renderMaps = function (data) { // data is an array of objects
+  for (let map of data) { // map is one object with one tweet
+    createMapElement(map);
+  }
+};
+
+//CREATE NEW MAP CONTAINER
+const createMapElement = function (map) {
+  const title = map.title;
+  const description = map.description;
+
+// HTML FOR A MAP CONTAINER
+$('.index-main').prepend(
+  `
+      <section class="main-section-box">Landmark
+        <div id="mapid-1" > ${title} </div>
+            <p>${description}</p>
+        </section>  `
+  );
+};
