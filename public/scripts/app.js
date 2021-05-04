9cc5590b59361d4b3d6fbe1a0f97559b3635998f
William Perry <durabilliam@gamil.com>
2021-05-04 16:37:56 -0230

    add initial maps to html

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -24,13 +24,40 @@ const renderMaps = function (data) { // data is an array of objects
 const createMapElement = function (map) {
   const title = map.title;
   const description = map.description;
+  const lat = map.lat;
+  const long = map.long;
+  const mapid = map.id
+  console.log("something", lat, long)
 
-// HTML FOR A MAP CONTAINER
-$('.index-main').prepend(
-  `
-      <section class="main-section-box">Landmark
-        <div id="mapid-1" > ${title} </div>
+
+
+
+
+  // HTML FOR A MAP CONTAINER
+  $('.index-main').prepend(
+    `<section>
+    <div id="map-${mapid}" style="width: 200px; height: 100px; position: relative;"></div>
+       <div id="mapid-1" > ${title} </div>
             <p>${description}</p>
-        </section>  `
+        </section> `
   );
+ console.log("ending", L);
+let maptest =  document.getElementById(`map-${mapid}`);
+console.log(maptest);
+console.log($(`.index-main`).html())
+let mymap = L.map(`map-${mapid}`).setView([lat, long], 12);
+            console.log("string", mymap);
+            L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+            attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+            maxZoom: 18,
+            id: 'mapbox/streets-v11',
+            tileSize: 512,
+            zoomOffset: -1,
+            accessToken: 'pk.eyJ1IjoiZHVyYWJpbGxpYW0iLCJhIjoiY2tvYTBtdXQ3Mm1odjJwcXd3MXkycmptcCJ9.NfmIqQQjSypgKHZciDx8rg'
+            }).addTo(mymap);
+
+
+
+
+
 };
