d6f262463e2c2baef5e4a4335a198680e21dc081
Nelli Butenko <butenkonelly93@gmail.com>
2021-05-05 23:54:48 +0000

    Clean up

--- pseudo.js
+++
@@ -1,7 +0,0 @@
-/*
-// from map page onClick refer to Point page
-// render the map By Id
-// form input => submit => extract data
-// title, description, photo INSERT in db
-// refer to new point page (with id)
-*/
