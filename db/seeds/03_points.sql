7d783068b5e9f225a27e3955174eb346fb717942
William Perry <durabilliam@gamil.com>
2021-05-05 13:19:04 -0230

    scss changes

--- db/seeds/03_points.sql
+++ db/seeds/03_points.sql
@@ -1,6 +1,6 @@
 -- points table seeds here
 INSERT INTO points (map_id, user_id, title, description, photo_url, lat, long)
-VALUES (1, 1, 'Soup Palace', 'The best French Onion Soup!!', 'https://image.shutterstock.com/image-photo/french-onion-gratin-soup-260nw-104052002.jpg', 53.13, 57.6),
+VALUES (1, 1, 'Soup Palace', 'The best French Onion Soup!!', 'https://image.shutterstock.com/image-photo/french-onion-gratin-soup-260nw-104052002.jpg', 21.311335, -157.8621),
 (2, 2, 'Sandwich Palace', 'The best Sandwich Ever!!', 'https://image.shutterstock.com/image-photo/two-fresh-submarine-sandwiches-ham-260nw-497930494.jpg', 53.13, 57.6),
 (3, 3, 'Chocloate Palace', 'The best Chocolate!!', 'https://image.shutterstock.com/image-photo/dark-chocolate-stack-chips-powder-260nw-230301934.jpg', 53.13, 57.6),
 (3, 1, 'Cheese R Us', 'Very Cheddery!!', 'https://image.shutterstock.com/image-photo/chedder-cheese-cracker-appetizer-tomato-600w-141846613.jpg', 53.13, 57.6);
