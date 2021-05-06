138e3c47dbefcccb10bd1458d976fb46247b6eb8
William Perry <durabilliam@gamil.com>
2021-05-06 15:34:40 -0230

    add to database

--- db/seeds/02_maps.sql
+++ db/seeds/02_maps.sql
@@ -1,8 +1,8 @@
 -- maps table seeds here
 INSERT INTO maps (user_id, title, description, lat, long)
-VALUES (1, 'Ottawa', 'This is a description', 45.4203, -75.6904),
-(1, 'Miami', 'This is a description', 25.7735, -80.1934),
-(2, 'Happy Adventure', 'This is a description', 48.6364, -53.7575),
-(3, 'New York', 'This is a description', 40.7302, -73.9895),
-(3, 'Dublin', 'This is a description', 53.3495, -6.2609),
-(3, 'Honolulu', 'This is a description', 21.3034, -157.8581);
+VALUES (1, 'Ottawa', 'Looking for good Maple Syrup??', 45.4203, -75.6904),
+(1, 'Miami', 'Pulled Pork at a Glance', 25.7735, -80.1934),
+(2, 'Happy Adventure', 'From the ocean to youe plate!!!', 48.6364, -53.7575),
+(3, 'New York', 'Wondering where to find a good bagel', 40.7302, -73.9895),
+(3, 'Dublin', 'Four Leaf Clovers.....', 53.3495, -6.2609),
+(3, 'Honolulu', 'Surf Board Sales', 21.3034, -157.8581);
